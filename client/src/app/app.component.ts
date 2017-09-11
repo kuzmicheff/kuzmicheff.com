1282f8b38d1a8399c3ef183e129e71caa41de100
kuzmicheff <kuzmicheff@gmail.com>
2017-09-11 10:11:55 -0500

    Tweaked styles and changed year to automatic value

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import {NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ NgbCarouselConfig ]
 })
 export class AppComponent {
-  title = 'app';
+
+  public title = 'kuzmicheff.com';
+  public currentDate = new Date();
+  public currentYear = this.currentDate.getFullYear();
+
+  constructor(
+    private config: NgbCarouselConfig
+  ) {
+    config.interval = 10000;
+    config.wrap = true;
+    config.keyboard = true;
+  }
 }
